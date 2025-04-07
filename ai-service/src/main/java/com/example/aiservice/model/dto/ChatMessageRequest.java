76e901e31175398b109de94d11fe918c7b880b87
Bowen <bzhao0106@gmail.com>
2025-04-07 14:06:49 +1000

    Initial commit

---
+++ ai-service/src/main/java/com/example/aiservice/model/dto/ChatMessageRequest.java
@@ -0,0 +1,9 @@
+package com.example.aiservice.model.dto;
+
+import java.util.List;
+
+public class ChatMessageRequest {
+    public List<AITextMessage> AITextMessages;
+    public String AllChatMessagesString;
+    public String Diagnosis;
+}
